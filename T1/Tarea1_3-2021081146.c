7cd82d51f4b8093c0a3fcfcd7fe0600de9dbfda3
Camilo Sanchez <92351898+Camilox0316@users.noreply.github.com>
2024-03-01 09:51:48 -0600

    Update Tarea1_3-2021081146.c

--- T1/Tarea1_3-2021081146.c
+++ T1/Tarea1_3-2021081146.c
@@ -1,33 +1,42 @@
+/*
+Cree un proceso padre que cree a su vez 5 procesos hijos los cuáles deben generar un número aleatorio que imprimirán 
+y luego retornarán al proceso padre como su estado de terminación (debe ser un número entero pequeño). 
+El padre también debe imprimir los números aleatorios que le retornaron sus procesos hijos al finalizar.
+*/
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+void print_num_random(int random_generado[], int n){
+    printf("Mis hijos generaron los siguientes numeros: \n");
+    for (int i = 0; i < n; i++){
+        printf("%d \n", random_generado[i]);
+    }
+    printf("\n");
+}
 
 void e3(){
-    int N_HIJOS = 5;
-    int exit_codes[N_HIJOS];
-    
-    for (int i = 0; i < N_HIJOS; i++){
+    int random_generado[5];
+    char nombre_proceso[] = {'A', 'B', 'C', 'D', 'E'};
+
+    for (int i = 0; i < 5; i++){
         int pid = fork();
         if (pid == 0){
             srand(getpid()); 
-            int n_random = rand() % 100; 
-            printf("Este es mi número random: %d \n", n_random);
-            exit(n_random); 
+            int my_random = rand() % 10; 
+            printf("Soy %c y mi numero generado es: %d \n", nombre_proceso[i] ,  my_random);
+            exit(my_random); 
         } else {
             
             int status;
             waitpid(pid, &status, 0); 
             if (WIFEXITED(status)){
-                exit_codes[i] = WEXITSTATUS(status); 
+                random_generado[i] = WEXITSTATUS(status); 
             }
         }
     }
-    printf("Los números random generados son: ");
-    for (int i = 0; i < N_HIJOS; i++){
-        printf("%d ", exit_codes[i]);
-    }
-    printf("\n");
+    print_num_random(random_generado, 5);
+
     exit(0);
 }
 int main() {
